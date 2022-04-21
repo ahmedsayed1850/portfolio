aa6c8e94fd0941302ca0af064b68d4fa42ebe1dd
Ahmed El Sayed <90536775+ahmedsayed1850@users.noreply.github.com>
2022-04-22 00:13:07 +0200

    grammar update

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -12,18 +12,14 @@ const Timeline = () => {
     <Section id="About">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        Front-End engineer with a proven ability to adaptin both self-starting
-        and collaborative environments while staying focused on achieving
-        high-quality results under strict deadlines.
+        Front-End engineer with a proven ability to adapt in both self-starting and collaborative environments while staying focused on achieving high-quality results under strict deadlines.
         <br />
         <br />
-        results-based approch and collaborate with cross-functional teams sales
-        and UX designers also when it come to backend I have knowledge and
-        what's going behind the scene.
+results-based approach and collaboration with cross-functional teams sales and UX designers also when it comes to the backend I have the knowledge and what's going behind the scene.
         <br />
         <br />
-        Eager to obtain a challenging position at innovative company like Dream
-        Version that will expand my learning and build upon my developer skills.
+Eager to obtain a challenging position at an innovative company like Dream Version that will expand my learning and build upon my developer skills.
+   
       </SectionText>
       <SectionDivider />
     </Section>
