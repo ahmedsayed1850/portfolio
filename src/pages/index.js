0882e3f550e90fcfcc419ee1ed280f988dd069d4
Ahmed El Sayed <90536775+ahmedsayed1850@users.noreply.github.com>
2022-07-07 06:45:33 +0200

    Added MS Clarity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
 import Hero from "../components/Hero/Hero";
+import Head from "next/head";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
@@ -8,6 +9,16 @@ import { Section } from "../styles/GlobalComponents";
 
 const Home = () => {
   return (
+   <>
+    <Head>
+    <script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "cnuz0dihgw");
+      </script>
+    </Head>
     <Layout>
       <Section grid>
         <Hero />
@@ -17,6 +28,7 @@ const Home = () => {
       <Technologies />
       <Timeline />
     </Layout>
+    </>
   );
 };
 
