9fc528506cc285b31e40c9ec5b5ea20e9a8e1bc5
Ahmed El Sayed <90536775+ahmedsayed1850@users.noreply.github.com>
2022-07-08 05:56:03 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
 import Hero from "../components/Hero/Hero";
-import Head from "next/head";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
@@ -9,16 +8,6 @@ import { Section } from "../styles/GlobalComponents";
 
 const Home = () => {
   return (
-   <>
-    <Head>
-    <script type="text/javascript">
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "cnuz0dihgw");
-      </script>
-    </Head>
     <Layout>
       <Section grid>
         <Hero />
@@ -28,7 +17,7 @@ const Home = () => {
       <Technologies />
       <Timeline />
     </Layout>
-    </>
+
   );
 };
 
